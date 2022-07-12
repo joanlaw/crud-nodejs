f8cd7a772cf5ea29e6c2819e80bb3ee54142ed7d
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-07-12 12:45:54 -0600

    se terminan de configurar metodos get put update delete

--- src/controllers/languaje.controller.js
+++ src/controllers/languaje.controller.js
@@ -14,13 +14,38 @@ try {
 
 };
 
+const getLanguage= async (request,response)=>{
+    try {   
+        console.log(request.params);
+
+        const { id } = request.params;
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, name, programmers FROM language WHERE id = ?", id);
+        console.log(result);
+        response.json(result);
+    } catch (error) {
+        response.status(500);
+        response.send(error.message);
+    }
+    
+    
+};
+
 const addLanguages= async (request,response)=>{
     try {
-        console.log(request.body);
+        const {name,programmers} = (request.body);
 
+        if (name == undefined || programmers == undefined) {
+            response.status(400).json ({message: "Por favor inserte datos"});
+        }
+
+        const language = {name,programmers};
+       
         const connection = await getConnection();
 
-        response.json("addLanguage");
+        const result= await connection.query("INSERT INTO language SET ?", language );
+
+        response.json({message: "Se insertaron los datos correctamente"});
 
     } catch (error) {
         response.status(500);
@@ -28,9 +53,51 @@ const addLanguages= async (request,response)=>{
     }
     
     
-    };
+};
+
+const updateLanguage= async (request,response)=>{
+        try {   
+            //console.log(request.params);
+            
+            const { id } = request.params;
+            const {name,programmers} = (request.body);
+
+            if (id == undefined || name == undefined || programmers == undefined) {
+                response.status(400).json ({message: "Por favor inserte datos"});
+            }
+
+            const language = {id, name, programmers};
+            const connection = await getConnection();
+            const result = await connection.query("UPDATE language SET ? WHERE id = ?", [language, id]);
+            console.log(result);
+            response.json(result);
+        } catch (error) {
+            response.status(500);
+            response.send(error.message);
+        }
+};     
+
+const deleteLanguage= async (request,response)=>{
+        try {   
+            console.log(request.params);
+    
+            const { id } = request.params;
+            const connection = await getConnection();
+            const result = await connection.query("DELETE FROM language WHERE id = ?", id);
+            console.log(result);
+            response.json(result);
+        } catch (error) {
+            response.status(500);
+            response.send(error.message);
+        }
+        
+        
+};    
 
 export const methods = {
     getLanguages,
-    addLanguages
+    getLanguage,
+    addLanguages,
+    updateLanguage,
+    deleteLanguage
 };
--- src/routes/lenguage.route.js
+++ src/routes/lenguage.route.js
@@ -4,6 +4,8 @@ import { methods as languageController } from "../controllers/languaje.controlle
 const router=Router();
 
 router.get("/", languageController.getLanguages);
+router.get("/:id", languageController.getLanguage);
 router.post("/", languageController.addLanguages);
-
+router.put("/:id", languageController.updateLanguage);
+router.delete("/:id", languageController.deleteLanguage);
 export default router;
